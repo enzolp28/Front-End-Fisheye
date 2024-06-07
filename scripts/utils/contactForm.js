ffd6e1f0b961334e4fe9199d70666640fcd9f2bc
enzolp28 <laposta.enzo@gmail.com>
2024-06-07 15:18:34 +0200

    modal scroll

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -11,13 +11,12 @@ function handleContactForm() {
   function displayModal() {
     body.setAttribute("aria-hidden", "true");
     body.setAttribute("tabindex", "-1");
-    modal.focus();
-    btnCloseModal.focus();
     headerPage.setAttribute("aria-hidden", "true");
     modal.setAttribute("aria-hidden", "false");
     body.classList.add("no-scroll");
     modal.style.display = "block";
-    console.log("appeler");
+    modal.focus();
+    btnCloseModal.focus();
   }
 
   function closeModal() {
